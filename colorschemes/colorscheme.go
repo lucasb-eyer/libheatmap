8a4db022266d55b91328629f5bbc2fc7436a2d70
lucasb-eyer <lucasb.eyer.be@gmail.com>
2013-10-13 10:15:04 +0200

    Colorscheme gen: Use template for C file too.

--- colorschemes/colorscheme.go
+++ colorschemes/colorscheme.go
@@ -38,7 +38,7 @@ const LICENSE string =
 
 `
 
-const HEADER_TEMPLATE string =
+const HEADER_TEMPLATE string = LICENSE +
 `#ifndef _HEATMAP_COLORSCHEMES_{{ToUpper .}}_H
 #define _HEATMAP_COLORSCHEMES_{{ToUpper .}}_H
 
@@ -61,6 +61,43 @@ extern const heatmap_colorscheme_t* heatmap_cs_{{.}}_mixed_exp;
 #endif
 
 #endif /* _HEATMAP_COLORSCHEMES_{{ToUpper .}}_H */
+`
+
+const SOURCE_TEMPLATE string = LICENSE +
+`#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "heatmap.h"
+#include "colorschemes/{{.Name}}.h"
+
+static const unsigned char discrete_data[] = {
+    0, 0, 0, 0{{.Discrete}}
+};
+static const heatmap_colorscheme_t discrete = { discrete_data, sizeof(discrete_data)/sizeof(discrete_data[0]/4) };
+const heatmap_colorscheme_t* heatmap_cs_{{.Name}}_discrete = &discrete;
+
+static const unsigned char soft_data[] = {
+    0, 0, 0, 0{{.Soft}}
+};
+static const heatmap_colorscheme_t soft = { soft_data, sizeof(soft_data)/sizeof(soft_data[0]/4) };
+const heatmap_colorscheme_t* heatmap_cs_{{.Name}}_soft = &soft;
+
+static const unsigned char mixed_data[] = {
+    0, 0, 0, 0{{.Mixed}}
+};
+static const heatmap_colorscheme_t mixed = { mixed_data, sizeof(mixed_data)/sizeof(mixed_data[0]/4) };
+const heatmap_colorscheme_t* heatmap_cs_{{.Name}}_mixed = &mixed;
+
+static const unsigned char mixed_exp_data[] = {
+    0, 0, 0, 0{{.MixedExp}}
+};
+static const heatmap_colorscheme_t mixed_exp = { mixed_exp_data, sizeof(mixed_exp_data)/sizeof(mixed_exp_data[0]/4) };
+const heatmap_colorscheme_t* heatmap_cs_{{.Name}}_mixed_exp = &mixed_exp;
+
+#ifdef __cplusplus
+}
+#endif
 `
 
 // This table contains the "keypoints" of the colorgradient you want to generate.
@@ -140,7 +177,7 @@ func CreateColorschemes(keypoints GradientTable, name string, w, h int) {
     }()
 
     // Might as well already create the header, we know all we need by now.
-    tmpl, err := template.New("test").Funcs(template.FuncMap{"ToUpper": strings.ToUpper}).Parse(LICENSE + HEADER_TEMPLATE)
+    tmpl, err := template.New("test").Funcs(template.FuncMap{"ToUpper": strings.ToUpper}).Parse(HEADER_TEMPLATE)
     if err != nil { log.Panic(err) }
     if err = tmpl.Execute(h_file, name); err != nil { log.Panic(err) }
 
@@ -148,46 +185,28 @@ func CreateColorschemes(keypoints GradientTable, name string, w, h int) {
     img_soft      := image.NewRGBA(image.Rect(0,0,w,h))
     img_mixed     := image.NewRGBA(image.Rect(0,0,w,h))
     img_mixed_exp := image.NewRGBA(image.Rect(0,0,w,h))
-
-    fmt.Fprint(c_file, LICENSE)
-    fmt.Fprintln(c_file, "#ifdef __cplusplus")
-    fmt.Fprintln(c_file, "extern \"C\" {")
-    fmt.Fprintln(c_file, "#endif\n")
-    fmt.Fprintln(c_file, `#include "heatmap.h"`)
-    fmt.Fprintf(c_file, `#include "colorschemes/%v.h"`+"\n\n", name)
+    discrete, soft, mixed, mixed_exp := "", "", "", ""
 
     // discrete
-    fmt.Fprintln(c_file, "static const unsigned char discrete_data[] = {")
-    fmt.Fprint(c_file, "    0, 0, 0, 0")
     for y, kp := range keypoints {
         r, g, b := kp.Col.RGB255()
-        fmt.Fprintf(c_file, ", %v, %v, %v, 255", r, g, b)
+        discrete += fmt.Sprintf(", %v, %v, %v, 255", r, g, b)
         draw.Draw(img_discrete, image.Rect(0, y, w, y+1), &image.Uniform{kp.Col}, image.ZP, draw.Src)
     }
-    fmt.Fprintln(c_file, "\n};")
-    fmt.Fprintln(c_file, "static const heatmap_colorscheme_t discrete = { discrete_data, sizeof(discrete_data)/sizeof(discrete_data[0]/4) };")
-    fmt.Fprintln(c_file, "const heatmap_colorscheme_t* heatmap_cs_" + name + "_discrete = &discrete;\n")
     savepng(img_discrete, name + "_discrete.png")
 
     // soft
-    fmt.Fprintln(c_file, "static const unsigned char soft_data[] = {")
-    fmt.Fprint(c_file, "    0, 0, 0, 0")
     for y := 0 ; y < h ; y++ {
         t := float64(y)/float64(h)
         a := uint8(math.Min(t*30.0, 1.0)*255.0)
         c := keypoints.GetInterpolatedColorFor(t)
         r, g, b := c.RGB255()
-        fmt.Fprintf(c_file, ", %v, %v, %v, %v", r, g, b, a)
+        soft += fmt.Sprintf(", %v, %v, %v, %v", r, g, b, a)
         draw.Draw(img_soft, image.Rect(0, y, w, y+1), &image.Uniform{c}, image.ZP, draw.Src)
     }
-    fmt.Fprintln(c_file, "\n};")
-    fmt.Fprintln(c_file, "static const heatmap_colorscheme_t soft = { soft_data, sizeof(soft_data)/sizeof(soft_data[0]/4) };")
-    fmt.Fprintln(c_file, "const heatmap_colorscheme_t* heatmap_cs_" + name + "_soft = &soft;\n")
     savepng(img_soft, name + "_soft.png")
 
     // mixed
-    fmt.Fprintln(c_file, "static const unsigned char mixed_data[] = {")
-    fmt.Fprint(c_file, "    0, 0, 0, 0")
     for y := 0 ; y < h ; y++ {
         t := float64(y)/float64(h)
         a := uint8(math.Min(t*30.0, 1.0)*255.0)
@@ -197,17 +216,12 @@ func CreateColorschemes(keypoints GradientTable, name string, w, h int) {
         c2 := keypoints.GetColorFor(t)
         c := c1.BlendRgb(c2, 0.2)
         r, g, b := c.RGB255()
-        fmt.Fprintf(c_file, ", %v, %v, %v, %v", r, g, b, a)
+        mixed += fmt.Sprintf(", %v, %v, %v, %v", r, g, b, a)
         draw.Draw(img_mixed, image.Rect(0, y, w, y+1), &image.Uniform{c}, image.ZP, draw.Src)
     }
-    fmt.Fprintln(c_file, "\n};")
-    fmt.Fprintln(c_file, "static const heatmap_colorscheme_t mixed = { mixed_data, sizeof(mixed_data)/sizeof(mixed_data[0]/4) };")
-    fmt.Fprintln(c_file, "const heatmap_colorscheme_t* heatmap_cs_" + name + "_mixed = &mixed;\n")
     savepng(img_mixed, name + "_mixed.png")
 
     // exp
-    fmt.Fprintln(c_file, "static const unsigned char mixed_exp_data[] = {")
-    fmt.Fprint(c_file, "    0, 0, 0, 0")
     for y := 0 ; y < h ; y++ {
         t := float64(y)/float64(h)
         a := uint8(math.Min(t*100.0, 1.0)*255.0) // TODO: Change a
@@ -217,15 +231,13 @@ func CreateColorschemes(keypoints GradientTable, name string, w, h int) {
         c2 := keypoints.GetColorFor(t)
         c := c1.BlendRgb(c2, 0.2)
         r, g, b := c.RGB255()
-        fmt.Fprintf(c_file, ", %v, %v, %v, %v", r, g, b, a)
+        mixed_exp += fmt.Sprintf(", %v, %v, %v, %v", r, g, b, a)
         draw.Draw(img_mixed_exp, image.Rect(0, y, w, y+1), &image.Uniform{c}, image.ZP, draw.Src)
     }
-    fmt.Fprintln(c_file, "\n};")
-    fmt.Fprintln(c_file, "static const heatmap_colorscheme_t mixed_exp = { mixed_exp_data, sizeof(mixed_exp_data)/sizeof(mixed_exp_data[0]/4) };")
-    fmt.Fprintln(c_file, "const heatmap_colorscheme_t* heatmap_cs_" + name + "_mixed_exp = &mixed_exp;\n")
     savepng(img_mixed_exp, name + "_mixed_exp.png")
 
-    fmt.Fprintln(c_file, "#ifdef __cplusplus")
-    fmt.Fprintln(c_file, "}")
-    fmt.Fprintln(c_file, "#endif")
+    // Write the C file now that we got all data.
+    tmpl, err = template.New("test").Parse(SOURCE_TEMPLATE)
+    if err != nil { log.Panic(err) }
+    if err = tmpl.Execute(c_file, struct { Name, Discrete, Soft, Mixed, MixedExp string }{ name, discrete, soft, mixed, mixed_exp }); err != nil { log.Panic(err) }
 }
